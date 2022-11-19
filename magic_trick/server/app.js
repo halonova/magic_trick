42eafda90758219f1d338e198decf6cddcf6c901
Beatriz Vilela <ana.beatriz@edu.unifor.br>
2022-11-18 22:23:43 -0300

    Add files via upload

---
+++ magic_trick/server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const server = express()
+const uuid = require('uuid');
+
+const port = 3000
+
+const items = require('./routes/items') 
+server.use("/items", items)
+
+//test 
+// server.get("/test", async (req, res) => {
+//     return res.status(200).json({ success: true })
+// })
+
+server.listen(port, () => {
+    console.log(`                                   
+    
+• ▌ ▄ ·.  ▄▄▄·  ▄▄ • ▪   ▄▄·     ▄▄▄▄▄▄▄▄  ▪   ▄▄· ▄ •▄ 
+·██ ▐███▪▐█ ▀█ ▐█ ▀ ▪██ ▐█ ▌▪    •██  ▀▄ █·██ ▐█ ▌▪█▌▄▌▪
+▐█ ▌▐▌▐█·▄█▀▀█ ▄█ ▀█▄▐█·██ ▄▄     ▐█.▪▐▀▀▄ ▐█·██ ▄▄▐▀▀▄·
+██ ██▌▐█▌▐█ ▪▐▌▐█▄▪▐█▐█▌▐███▌     ▐█▌·▐█•█▌▐█▌▐███▌▐█.█▌
+▀▀  █▪▀▀▀ ▀  ▀ ·▀▀▀▀ ▀▀▀·▀▀▀      ▀▀▀ .▀  ▀▀▀▀·▀▀▀ ·▀  ▀           
+             • Listening on port`, `${port}`)
+})
---
+++ magic_trick/server/routes/items.js
@@ -0,0 +1,99 @@
+const express = require("express")
+const router = express.Router()
+
+// const uuid = require('uuid');
+// id = uuid.v1()
+
+var items = [{
+    name: "Floresta",
+    price: "1.50",
+    is_wishlist: false
+},
+{
+    name: "Planície",
+    price: "1.50",
+    is_wishlist: false
+},
+{
+    name: "Ilha",
+    price: "1.50",
+    is_wishlist: false
+},
+{
+    name: "Pântano",
+    price: "1.50",
+    is_wishlist: false
+},
+{
+    name: "Montanha",
+    price: "1.50",
+    is_wishlist: false
+}
+]
+
+//return item
+router.get('/:index', (req, res) => {
+    try {
+        const { index } = req.params;
+        return res.json(items[index]);
+    } catch {
+        res.status(404).send("Not Found")
+    }
+})
+
+//return by name
+router.get('/:name', (req, res) => {
+    try {
+        //res.send("OK")
+    } catch {
+        res.status(404).send("Not Found")
+    }
+})
+
+//return all items
+router.get('/', async (req, res) => {
+    try {
+        return res.json(items)
+    } catch {
+        res.status(404).send("Not Found")
+    }
+})
+
+//create items
+router.post('/', (req, res) => {
+    try {
+        const { name } = req.body
+        items.push(name)
+
+        return res.status(201).json(items)
+    } catch {
+        res.status(404).send("Not Found")
+    }
+})
+
+//update
+router.put('/items/:index', (req, res) => {
+    try {
+        const { index } = req.params
+        const { name } = req.params
+
+        items[index] = name
+
+        return res.json(items)
+    } catch {
+        res.status(404).send("Not Found")
+    }
+})
+
+//delete
+router.delete('/items/:index', (req, res) => {
+    try {
+        const { item } = req.params;
+
+        items.splice(index, 1)
+        return res.json({ message: "O item foi deletado" })
+    } catch {
+
+    }
+})
+module.exports = router;
